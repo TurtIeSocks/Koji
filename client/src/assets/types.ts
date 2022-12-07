d1eb5030cd5d252f9a2325296454e13bbe4a4c4c
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2022-12-07 00:17:12 -0500

    last bit
    + bit of cheating until the leaflet.locatecontrol types are fixed

--- client/src/assets/types.ts
+++ client/src/assets/types.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-namespace */
+
 import type { Feature, FeatureCollection } from 'geojson'
+import * as L from 'leaflet'
 
 import type { UseStore } from '@hooks/useStore'
 import type { UseStatic } from '@hooks/useStatic'
@@ -72,3 +76,68 @@ export type ToConvert =
   | FeatureCollection
   | string
   | Poracle
+
+// for some reason locate.control @types augmentation is broken
+declare module 'leaflet' {
+  namespace Control {
+    class Locate extends Control {
+      constructor(locateOptions?: LocateOptions)
+      onAdd(map: Map): HTMLElement
+      start(): void
+      stop(): void
+      stopFollowing(): void
+      setView(): void
+    }
+    interface LocateOptions {
+      position?: string | undefined
+      layer?: Layer | undefined
+      setView?: boolean | string | undefined
+      keepCurrentZoomLevel?: boolean | undefined
+      initialZoomLevel?: number | boolean | undefined
+      flyTo?: boolean | undefined
+      clickBehavior?: any
+      returnToPrevBounds?: boolean | undefined
+      cacheLocation?: boolean | undefined
+      drawCircle?: boolean | undefined
+      drawMarker?: boolean | undefined
+      showCompass?: boolean | undefined
+      markerClass?: any
+      compassClass?: any
+      circleStyle?: PathOptions | undefined
+      markerStyle?: PathOptions | MarkerOptions | undefined
+      compassStyle?: PathOptions | undefined
+      followCircleStyle?: PathOptions | undefined
+      followMarkerStyle?: PathOptions | undefined
+      icon?: string | undefined
+      iconLoading?: string | undefined
+      iconElementTag?: string | undefined
+      textElementTag?: string | undefined
+      circlePadding?: number[] | undefined
+      metric?: boolean | undefined
+      createButtonCallback?:
+        | ((container: HTMLDivElement, options: LocateOptions) => void)
+        | undefined
+      onLocationError?:
+        | ((event: ErrorEvent, control: Locate) => void)
+        | undefined
+      onLocationOutsideMapBounds?: ((control: Locate) => void) | undefined
+      showPopup?: boolean | undefined
+      strings?: StringsOptions | undefined
+      locateOptions?: L.LocateOptions | undefined
+    }
+    interface StringsOptions {
+      title?: string | undefined
+      metersUnit?: string | undefined
+      feetUnit?: string | undefined
+      popup?: string | undefined
+      outsideMapBoundsMsg?: string | undefined
+    }
+  }
+
+  namespace control {
+    /**
+     * Creates a Leaflet.Locate control
+     */
+    function locate(options?: Control.LocateOptions): Control.Locate
+  }
+}
