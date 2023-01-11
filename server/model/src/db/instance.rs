b64d4a982b90d5bc70cb6ff47490cf03e4a15528
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-01-11 17:14:39 -0500

    Update instance.rs

--- server/model/src/db/instance.rs
+++ server/model/src/db/instance.rs
@@ -88,6 +88,7 @@ impl Query {
     ) -> Result<Feature, DbErr> {
         let items = Entity::find()
             .filter(Column::Name.eq(Value::String(instance_name.to_string())))
+            .filter(Column::Name.eq(instance_name.trim().to_string()))
             .one(conn)
             .await?;
         if let Some(items) = items {
