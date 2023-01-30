5ac2a69f1bbf99642b07ce7362bebffbf01ba84c
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-01-30 09:24:56 -0500

    Revert "drawer tab hovering expansion"
    This reverts commit ed54670ec5fe342cc4021f070f0ae4e7ad7499f7.

--- client/src/components/drawer/index.tsx
+++ client/src/components/drawer/index.tsx
@@ -47,7 +47,6 @@ export default function DrawerIndex({ drawerWidth }: Props) {
   const { setStore } = usePersist.getState()
 
   const [tab, setTab] = React.useState(TABS.indexOf(menuItem) || 0)
-  const [hover, setHover] = React.useState(false)
 
   const toggleDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
     if (
@@ -86,7 +85,7 @@ export default function DrawerIndex({ drawerWidth }: Props) {
     <Drawer
       variant="permanent"
       open={drawer}
-      drawerWidth={drawerWidth + (hover ? 55 : 0)}
+      drawerWidth={drawerWidth}
       onClose={toggleDrawer}
     >
       {drawer ? (
@@ -103,34 +102,19 @@ export default function DrawerIndex({ drawerWidth }: Props) {
           >
             <Tabs
               orientation="vertical"
+              variant="scrollable"
               value={tab}
               onChange={handleChange}
-              sx={{
-                borderRight: 1,
-                borderColor: 'divider',
-              }}
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
+              sx={{ borderRight: 1, borderColor: 'divider' }}
             >
               {TABS.map((text, i) => {
                 const Icon = ICON_MAP[text]
-
                 return (
                   <Tab
                     key={text}
                     value={i}
-                    label={hover ? text : ''}
-                    icon={hover ? undefined : <Icon />}
-                    sx={(theme) => ({
-                      p: 0,
-                      m: 0,
-                      fontSize: '0.8rem',
-                      minWidth: hover ? 100 : 45,
-                      transition: theme.transitions.create('width', {
-                        easing: theme.transitions.easing.sharp,
-                        duration: theme.transitions.duration.leavingScreen,
-                      }),
-                    })}
+                    icon={<Icon />}
+                    sx={{ p: 0, m: 0, minWidth: 45 }}
                   />
                 )
               })}
