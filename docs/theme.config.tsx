d75022bfbbcad6f5a461dc7918b41a021caf8861
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-04-01 00:32:15 -0400

    tag test

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -3,13 +3,29 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Kōji</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Kōji" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:description"
+        content="Rust based geofence editor, manager, and distributor"
+      />
+      <meta
+        property="og:image"
+        content="https://github.com/TurtIeSocks/Koji/blob/main/client/public/favicon.png"
+      />
+    </>
+  ),
   project: {
     link: 'https://github.com/TurtIeSocks/Koji',
   },
   chat: {
     link: 'https://discord.gg/EYYsKPVawn',
   },
-  docsRepositoryBase: 'https://github.com/TurtIeSocks/Koji',
+  docsRepositoryBase:
+    'https://github.com/TurtIeSocks/Koji/tree/main/docs/pages',
   footer: {
     text: 'Kōji Docs',
   },
