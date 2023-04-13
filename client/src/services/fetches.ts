e56a1387eaa38933481d739f8fe120ec2f268bfa
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-04-13 10:37:29 -0400

    remove extra `}`

--- client/src/services/fetches.ts
+++ client/src/services/fetches.ts
@@ -270,7 +270,7 @@ export async function clusteringRouting(): Promise<FeatureCollection> {
       return {
         id: `${area.id.toString().split('__')[0]}__${getRouteType(category)}__${
           fenceRef || routeRef ? 'KOJI' : 'CLIENT'
-        }}`,
+        }`,
         ...json.data,
       }
     }),
