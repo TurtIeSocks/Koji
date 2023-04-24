cceb909e13e9e4c998b8faada7ffc6c3227f14f9
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-04-24 14:46:43 -0400

    catch tileserver map errors

--- client/src/pages/admin/tileserver/TileServerMap.tsx
+++ client/src/pages/admin/tileserver/TileServerMap.tsx
@@ -8,18 +8,22 @@ export default function TileServerMap({
 }: {
   formData: KojiTileServer
 }) {
-  return (
-    <Map
-      key={formData.url}
-      renderOwnTileLayer
-      style={{ width: '100%', height: '50vh' }}
-    >
-      <TileLayer
-        url={
-          formData.url ||
-          'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png'
-        }
-      />
-    </Map>
-  )
+  try {
+    return (
+      <Map
+        key={formData.url}
+        renderOwnTileLayer
+        style={{ width: '100%', height: '50vh' }}
+      >
+        <TileLayer
+          url={
+            formData.url ||
+            'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png'
+          }
+        />
+      </Map>
+    )
+  } catch (e) {
+    return <div>URL is invalid, could not load the preview</div>
+  }
 }
