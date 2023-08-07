d7baa7a5a16aa30f32c5931f57c4f080f2e9ec9a
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-06 22:58:58 -0400

    fix: improved project api support

--- server/api/src/public/v1/project.rs
+++ server/api/src/public/v1/project.rs
@@ -25,7 +25,7 @@ async fn push_to_prod(
 
     let features = geofence::Query::project_as_feature(
         &conn.koji_db,
-        id,
+        id.clone(),
         &ApiQueryArgs {
             name: Some(true),
             mode: Some(true),
@@ -35,30 +35,38 @@ async fn push_to_prod(
     .await
     .map_err(actix_web::error::ErrorInternalServerError)?;
 
-    let (inserts, updates) = if scanner_type == "rdm" {
-        instance::Query::upsert_from_geometry(
-            &conn.data_db,
-            GeoFormats::FeatureVec(features),
-            false,
-        )
-        .await
-    } else {
-        area::Query::upsert_from_geometry(
-            &conn.unown_db.as_ref().unwrap(),
-            GeoFormats::FeatureVec(features),
-        )
-        .await
-    }
-    .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    let project = project::Query::get_scanner_project(&conn.koji_db)
+    let project = project::Query::get_one(&conn.koji_db, id)
         .await
         .map_err(actix_web::error::ErrorInternalServerError)?;
-    if let Some(project) = project {
-        send_api_req(project, Some(scanner_type))
+
+    log::error!("Scanner Type: {:?}", project);
+    let (inserts, updates) = if project.scanner {
+        if scanner_type == "rdm" {
+            instance::Query::upsert_from_geometry(
+                &conn.data_db,
+                GeoFormats::FeatureVec(features),
+                false,
+            )
             .await
-            .map_err(actix_web::error::ErrorInternalServerError)?;
-    }
+        } else {
+            area::Query::upsert_from_geometry(
+                &conn.unown_db.as_ref().unwrap(),
+                GeoFormats::FeatureVec(features),
+            )
+            .await
+        }
+        .map_err(actix_web::error::ErrorInternalServerError)?
+    } else {
+        (0, 0)
+    };
+    let scanner_type = if project.scanner {
+        scanner_type.to_string()
+    } else {
+        String::from("other")
+    };
+    send_api_req(project, Some(&scanner_type))
+        .await
+        .map_err(actix_web::error::ErrorInternalServerError)?;
     log::info!("Rows Updated: {}, Rows Inserted: {}", updates, inserts);
 
     Ok(HttpResponse::Ok().json(Response {
