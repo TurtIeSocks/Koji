a841fad930de17778d795ea87686890eaaef1e25
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2025-06-26 09:09:58 -0400

    Update s2.rs

--- server/algorithms/src/clustering/s2.rs
+++ server/algorithms/src/clustering/s2.rs
@@ -1,11 +1,9 @@
 use geojson::Feature;
-use hashbrown::HashSet;
+use hashbrown::HashMap;
 use model::api::single_vec::SingleVec;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use s2::cellid::CellID;
 
 use crate::bootstrap;
-use crate::s2::cell_coverage;
 
 pub fn cluster(
     feature: Feature,
@@ -17,59 +15,35 @@ pub fn cluster(
     let bootstrap_cells = bootstrap::s2::BootstrapS2::new(&feature, level as u64, size);
     let all_cells = bootstrap_cells.result();
 
-    let valid_cells = data
-        .iter()
-        .map(|f| {
-            CellID::from(s2::latlng::LatLng::from_degrees(f[0], f[1]))
-                .parent(level as u64)
-                .0
-        })
-        .collect::<HashSet<u64>>();
+    let mut cell_map = HashMap::<u64, usize>::new();
 
-    let filtered_cells: Vec<[f64; 2]> = all_cells
-        .into_par_iter()
+    data.iter().for_each(|f| {
+        cell_map
+            .entry(
+                CellID::from(s2::latlng::LatLng::from_degrees(f[0], f[1]))
+                    .parent(level as u64)
+                    .0,
+            )
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+    });
+
+    all_cells
+        .into_iter()
         .filter_map(|point| {
-            let coverage = cell_coverage(point[0], point[1], size, level)
-                .lock()
-                .unwrap()
-                .iter()
-                .map(|c| c.clone())
-                .collect::<Vec<_>>()
-                .clone();
-
-            let contains_count = data
-                .iter()
-                .filter_map(|f| {
-                    let b = CellID::from(s2::latlng::LatLng::from_degrees(f[0], f[1]))
-                        .parent(level as u64)
-                        .0;
-
-                    if coverage.contains(&b) {
-                        Some(1)
-                    } else {
-                        None
-                    }
-                })
-                .count();
-
-            println!("Contains count: {0}", contains_count);
-
-            if contains_count < min_points {
-                return None;
-            }
-
-            if cell_coverage(point[0], point[1], size, level)
-                .lock()
-                .unwrap()
-                .iter()
-                .any(|c| valid_cells.contains(c))
-            {
-                Some(point)
+            if let Some(&count) = cell_map.get(
+                &CellID::from(s2::latlng::LatLng::from_degrees(point[0], point[1]))
+                    .parent(level as u64)
+                    .0,
+            ) {
+                if count >= min_points {
+                    Some(point)
+                } else {
+                    None
+                }
             } else {
                 None
             }
         })
-        .collect();
-
-    filtered_cells
+        .collect()
 }
