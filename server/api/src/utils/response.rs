063d61fe907e9fd6ccabc26bc0467818f4a17776
BigFun <7109934+kbtbc@users.noreply.github.com>
2026-09-09 20:35:39 +0000

    feat: expose carto_api_key on ConfigResponse

--- server/api/src/utils/response.rs
+++ server/api/src/utils/response.rs
@@ -17,6 +17,7 @@ pub struct ConfigResponse {
     pub start_lat: Precision,
     pub start_lon: Precision,
     pub tile_server: String,
+    pub carto_api_key: String,
     pub scanner_type: ScannerType,
     pub logged_in: bool,
     pub dangerous: bool,
